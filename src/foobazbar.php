8253a010acc9ad663c52ff2ad240e442489735af
Brad Bell <brad@pixelandtonic.com>
2022-06-29 22:11:10 -0700

    cron debug

--- src/foobazbar.php
+++ src/foobazbar.php
@@ -1,4 +1,4 @@
 <?php
 
-$output = shell_exec("env");
+$output = shell_exec("source /opt/elasticbeanstalk/deployment/envvars;env");
 file_put_contents('/var/app/current/cron_env_shell_dump.txt', $output);
