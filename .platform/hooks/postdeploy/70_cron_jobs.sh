53c8f518078df67986eb4b145dce6978d41692d2
Brad Bell <brad@pixelandtonic.com>
2022-06-29 21:55:46 -0700

    cron debug

--- .platform/hooks/postdeploy/70_cron_jobs.sh
+++ .platform/hooks/postdeploy/70_cron_jobs.sh
@@ -6,4 +6,4 @@ mkdir -p /home/webapp ; chown -R webapp:webapp /home/webapp
 # Create CRON files
 
 # After the deployment finishes, set up a Crontab for craftnet
-echo "* * * * * webapp bash -c "\"" . <(source /opt/elasticbeanstalk/deployment/envvars) && php /var/app/current/craft schedule/run --scheduleFile=@config/schedule.php 1>> /dev/null 2>&1"\"" " | sudo tee /etc/cron.d/craftnet
+echo "* * * * * webapp bash -c "\"" . <(source /opt/elasticbeanstalk/deployment/envvars) && php /var/app/current/src/foobazbar.php 1>> /dev/null 2>&1"\"" " | sudo tee /etc/cron.d/craftnet
