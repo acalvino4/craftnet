d503b93541d95e19c3663090ae6fc63f73f2c116
Benjamin David <ben@pixelandtonic.com>
2022-08-25 13:27:26 +0200

    Added back to cart link to checkout layout

--- web/craftnetresources/src/console/js/components/layouts/CheckoutLayout.vue
+++ web/craftnetresources/src/console/js/components/layouts/CheckoutLayout.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="w-full max-w-screen-lg m-auto p-4 md:p-8">
-    <router-view :key="$route.path"></router-view>
+  <div>
+    <div>
+      <div class="w-full max-w-screen-lg m-auto p-4 md:p-8">
+        <router-link to="/cart">← Back to cart</router-link>
+      </div>
+    </div>
+    <div class="w-full max-w-screen-lg m-auto p-4 md:p-8">
+      <router-view :key="$route.path"></router-view>
+    </div>
   </div>
 </template>
