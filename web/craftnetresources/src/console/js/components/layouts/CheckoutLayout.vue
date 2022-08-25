42680a8375b4d76198b354b4c3bbfa83f8924539
Benjamin David <ben@pixelandtonic.com>
2022-08-25 13:36:59 +0200

    Improved checkout layout

--- web/craftnetresources/src/console/js/components/layouts/CheckoutLayout.vue
+++ web/craftnetresources/src/console/js/components/layouts/CheckoutLayout.vue
@@ -5,7 +5,7 @@
         <router-link to="/cart">← Back to cart</router-link>
       </div>
     </div>
-    <div class="w-full max-w-screen-lg m-auto p-4 md:p-8">
+    <div class="w-full max-w-screen-lg m-auto p-4 md:px-8 md:pt-8 md:pb-20">
       <router-view :key="$route.path"></router-view>
     </div>
   </div>
