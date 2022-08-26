3374ba1ef02c8f579280eb47488c80c101614ddb
Benjamin David <ben@pixelandtonic.com>
2022-08-26 18:38:47 +0200

    Improved address card

--- web/craftnetresources/src/console/js/components/billing/addresses/AddressCard.vue
+++ web/craftnetresources/src/console/js/components/billing/addresses/AddressCard.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="border border-gray-200 dark:border-gray-700 rounded-md p-6 flex flex-col">
     <div class="flex-1">
-      <div class="font-mono">{{address.id}}</div>
+      <div class="text-xs font-mono text-gray-500">#{{address.id}}</div>
 
       <div class="mt-2 space-x-2">
         <badge type="info">{{address.title}}</badge>
@@ -18,7 +18,6 @@
     </div>
 
     <div class="mt-4 space-x-4">
-      <a href="#" @click.prevent="$emit('setPrimary')">Set as primary</a>
       <a href="#" @click.prevent="$emit('edit')">Edit</a>
       <a href="#" @click.prevent="$emit('remove')">Remove</a>
     </div>
