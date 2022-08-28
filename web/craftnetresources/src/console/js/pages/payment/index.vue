1cf8f170fc6c9fb165cbdcbd963a44001d5f382a
Benjamin David <ben@pixelandtonic.com>
2022-08-28 15:40:27 +0200

    Use input radios instead of dropdown to select an address

--- web/craftnetresources/src/console/js/pages/payment/index.vue
+++ web/craftnetresources/src/console/js/pages/payment/index.vue
@@ -65,11 +65,36 @@
         <div class="mt-8">
           <h2>Billing Address</h2>
 
-          <div class="mt-4">
-            <dropdown
-              :options="addressOptions"
-              v-model="billingAddressId"
-            />
+          <div class="mt-4 space-y-4">
+            <template v-for="address in addresses">
+              <div class="flex border-b py-4">
+                <div class="mt-1 mr-2">
+                  <input
+                    :id="'address-' + address.id"
+                    type="radio"
+                    :value="address.id"
+                    v-model="billingAddressId"
+                  />
+                </div>
+                <label class="flex flex-1" :for="'address-' + address.id">
+                  <div class="flex-1">
+                    <div class="flex items-center space-x-2">
+                      <div v-if="address.countryCode">{{address.countryCode}}</div>
+                      <div v-if="address.addressLine1">{{address.addressLine1}}</div>
+                      <div v-if="address.locality">{{address.locality}}</div>
+                      <div v-if="address.postalCode">{{address.postalCode}}</div>
+                    </div>
+                    <div class="text-sm font-mono text-gray-500">#{{address.id}}</div>
+                  </div>
+                  <div>
+                    <a href="#">Edit</a>
+                  </div>
+                </label>
+              </div>
+            </template>
+            <div>
+              <a href="#">+ Add a new billing address</a>
+            </div>
           </div>
         </div>
       </template>
@@ -235,7 +260,23 @@ export default {
 
       return options
     },
+
+    addressRadioOptions() {
+      const options = []
+
+      if (this.addresses) {
+        this.addresses.forEach(address => {
+          options.push({
+            label: '#' + address.id,
+            value: address.id,
+          })
+        })
+      }
+
+      return options
+    },
   },
+
 
   methods: {
     pay() {
